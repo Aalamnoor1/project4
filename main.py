cbb997583a9a9dbd35777191e4e9e41852785ea1
Aaa <Aaa@gmail.com>
2023-09-03 18:31:27 +0530

    new branch added

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def print_hi(name):
-
     print(f'Hi,{name}')
-    if __name__ == '__main__':
-        name = 'try2vatch'
-        address = 'india'
-        print_hi(f'new word, {name}')
+
+
+if __name__ == '__main__':
+    name = 'try2vatch'
+    address = 'india'
+    print_hi(f'new word, {name}')
